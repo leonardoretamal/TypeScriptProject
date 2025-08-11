e88bd6e64cc94e40ec54035cbf5984fba8d46fbd
Leonardo Retamal <leonardo.retamal.morales@gmail.com>
2025-08-11 15:10:34 -0400

    creación de desestructuración de un objeto.

---
+++ src/topics/05-basic-destructuring.ts
@@ -0,0 +1,45 @@
+interface AudioPlayer {
+  audioVolume: number;
+  songDuration: number;
+  song: string;
+  details: Details;
+}
+
+interface Details {
+  author: string;
+  year: number;
+}
+
+// ctrl + . para ver sugerencias.
+const audioPlayer: AudioPlayer = {
+  audioVolume: 90,
+  songDuration: 30,
+  song: "Specter",
+  details: {
+    author: "Bad Omens",
+    year: 2025,
+  },
+};
+
+const song = "New Song";
+
+// desestructuración de un objeto
+const {
+  song: AnothersSong,
+  songDuration: duration,
+  details, //pasamos el objeto details completo
+  //  details: { author }, facil perderse con esta sintaxis
+} = audioPlayer;
+
+//y luego podemos acceder a sus propiedades
+const { author, year } = details;
+
+console.log("Song: ", AnothersSong);
+console.log("Duration: ", duration);
+console.log("Author: ", author);
+console.log("Year: ", year);
+//ejemplos sin destructuring
+/* console.log(audioPlayer.song);
+console.log(audioPlayer.details.author); */
+
+export {};
