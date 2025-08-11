d535ca091f4b15fcec93d2befc27e1d3f3a3c8c8
Leonardo Retamal <leonardo.retamal.morales@gmail.com>
2025-08-11 14:14:53 -0400

    archivo function explicacion

--- src/topics/03-functions.ts
+++ src/topics/03-functions.ts
@@ -2,13 +2,9 @@ function addNumbers(a: number, b: number) {
   return a + b;
 }
 
-const result: number = addNumbers(1, 2);
-
 const addNumbersArrow = (a: number, b: number): string => {
   return `${a + b}`; //nos perite hacer una inyeccion literal
 };
-
-const result2: string = addNumbersArrow(3, 4);
 
 function multiply(
   firstNumber: number,
@@ -18,8 +14,33 @@ function multiply(
   return firstNumber * base;
 } // se recomienda primero poner los obligatorios y luego los opcionales y por ultimo los por defecto
 
+/* const result: number = addNumbers(1, 2);
+const result2: string = addNumbersArrow(3, 4);
 const result3: number = multiply(5);
+console.log({ result, result2, result3 }); */
 
-console.log({ result, result2, result3 });
+interface Character {
+  name: string;
+  hp: number;
+  //en una interface se puede definir un metodo con una funcion flecha
+  showHp: () => void;
+}
+
+const healCharacter = (character: Character, amount: number) => {
+  character.hp += amount;
+};
+
+const strider: Character = {
+  name: "Strider",
+  hp: 50,
+  showHp() {
+    console.log(`Puntos de vida ${this.hp}`);
+  },
+};
+healCharacter(strider, 10);
+healCharacter(strider, 30);
+healCharacter(strider, 80);
+
+strider.showHp();
 
 export {};
