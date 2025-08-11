1e34b4e0c487e4af38ddde0730e41477f785f74f
Leonardo Retamal <leonardo.retamal.morales@gmail.com>
2025-08-11 19:14:46 -0400

    Extender una clase

--- src/topics/08-clases.ts
+++ src/topics/08-clases.ts
@@ -2,19 +2,24 @@
 export class Person {
   //son variables pero como estan dentro de la clase se les conoce como
   //properties
- /*  public name: string;
+  /*  public name: string;
   private address: string; */
 
   //y las funciones dentro de la clase se conocen como métodos.
+  constructor(public name: string, private address: string = "No address") {}
+}
+
+export class Hero extends Person {
   constructor(
-    public name: string,
-    private address: string = 'No address'
-    ) {
-    
+    public alterEgo: string,
+    public age: number,
+    public realName: string
+  ) {
+    super(realName, "New York");
   }
 }
 
-const ironman = new Person("Iron Man");
+const ironman = new Hero("Iron Man", 45, "Tony");
 
 /* console.log(ironman.addres); */
 console.log(ironman);
